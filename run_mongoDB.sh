96d30ca8ac2441db7ce5426fc534779c65f78e46
lj1218 <lj_ebox@163.com>
2019-11-21 22:06:37 +0800

    Optimize run_mongoDB.sh

--- run_mongoDB.sh
+++ run_mongoDB.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-docker pull mongo
-docker rm -f mongoDB
-docker run -p 27017:27017 --name mongoDB -d mongo
+image=mongo
+container_name=mongoDB
+args="-p 27017:27017"
+
+docker inspect ${image} -f '{{.Id}}' >/dev/null 2>&1 || docker pull ${image}
+docker rm -f ${container_name} >/dev/null 2>&1
+docker run -d --name ${container_name} ${args} ${image}
